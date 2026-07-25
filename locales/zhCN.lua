63e764bfe6f53033cf2295364a652385ba8d4f9d
sdahl <wuildz@gmail.com>
2026-07-25 19:01:39 +0200

    Added zhCN locales, (thanks OneManWow)

--- locales/zhCN.lua
+++ locales/zhCN.lua
@@ -1,5 +1,49 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("ExtraStats", "zhCN")
 if not L then
-    return
+	return
 end
 
+L["stats.base"] = "基础"
+L["stats.attributes"] = "属性"
+L["stats.melee"] = "近战"
+L["stats.ranged"] = "远程"
+L["stats.spell"] = "法术"
+L["stats.defense"] = "防御"
+L["stats.enhancements"] = "强化"
+
+L["stats.health"] = "生命值"
+L["stats.movespeed"] = "速度"
+L["stats.rage"] = "怒气"
+L["stats.energy"] = "能量"
+L["stats.mana"] = "法力值"
+L["stats.runic_power"] = "符文能量"
+
+L["stats.tooltip.movementspeed"] = "移动速度"
+L["stats.tooltip.movementspeed_description"] = "你当前的移动速度"
+
+L["stats.attributes.strength"] = "力量"
+L["stats.attributes.agility"] = "敏捷"
+L["stats.attributes.stamina"] = "耐力"
+L["stats.attributes.intellect"] = "智力"
+L["stats.attributes.spirit"] = "精神"
+
+L["stats.damage"] = "伤害"
+L["stats.speed"] = "速度"
+L["stats.power"] = "强度"
+L["stats.hit_chance"] = "命中几率"
+L["stats.crit_chance"] = "暴击几率"
+L["stats.haste_rating"] = "急速等级"
+L["stats.regen"] = "法力回复"
+L["stats.penetration"] = "穿透"
+
+L["stats.bonus_damage"] = "伤害加成"
+L["stats.bonus_healing"] = "治疗加成"
+
+L["stats.armor"] = "护甲"
+L["stats.defense"] = "防御"
+L["stats.dodge"] = "闪避"
+L["stats.block"] = "格挡"
+L["stats.parry"] = "招架"
+
+L["stats.expertise"] = "精准"
+L["stats.resilience"] = "韧性"
